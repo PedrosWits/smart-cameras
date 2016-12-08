2f00686c0ab532bf7505d34ab39f0f9001f17942
Pedro Pinto da Silva (PGR) <ppintodasilva@gmail.com>
2016-12-08 13:21:22 +0000

    Finished vehicle w/ tests

--- smartcameras/test/test_vehicle.py
+++ smartcameras/test/test_vehicle.py
@@ -1,5 +1,12 @@
 import smartcameras.vehicle as vehicle
+import random
+import numpy as np
+import math
 
 def test_vehicle():
-    print ""
-    vehicle.NormalVehicle(30).dump()
+    random.seed(1337)
+    np.random.seed(1337)
+    truck = vehicle.NormalVehicle(30)
+    assert truck.plate == "QN35 EVJ"
+    assert truck.type == "TRUCK"
+    assert math.floor(truck.speed) == 27
--- smartcameras/vehicle.py
+++ smartcameras/vehicle.py
@@ -10,27 +10,25 @@ class NormalVehicle(object):
         if(maximumSpeed < 30):
             raise ValueError('Maximum speed should be at least 30 km per hour')
         # Uniformly sample from from upper case letters and numbers
-        self.plate = random.choice(string.ascii_uppercase) + \
-                     random.choice(string.ascii_uppercase) + \
-                     random.choice(string.digits) + \
-                     random.choice(string.digits) + \
-                     " " + \
-                     random.choice(string.ascii_uppercase) + \
-                     random.choice(string.ascii_uppercase) + \
-                     random.choice(string.ascii_uppercase)
+        self.plate = genLetter() + genLetter() + \
+                     genNumber() + genNumber() + " " + \
+                     genLetter() + genLetter() + genLetter()
         # Uniformly sample from vehicle type
         index = np.random.randint(0, len(VehicleType))
         self.type = VehicleType[index]
         # Sample vehicle speed from a normal distribution
-        #  from which mu and sigma are obtained from the
-        #  following system of equations:
-        #       mu + sigma = maximumSpeed
-        #       mu - 3sgima = 10
-        sigma = (maximumSpeed - 10) / 4
-        mu = maximumSpeed - sigma
+        #  centered at maximumSpeed with sigma = 1/5 of maximumSpeed
+        sigma = maximumSpeed / 5
+        mu = maximumSpeed
         self.speed = np.random.normal(mu, sigma)
 
     def dump(self):
-        print "License plate = " , self.plate
-        print "Type = " , self.type
-        print "Speed = " , self.speed
+        print "License plate =" , self.plate
+        print "Type =" , self.type
+        print "Speed =" , self.speed
+
+def genNumber():
+    return random.choice(string.digits)
+
+def genLetter():
+    return random.choice(string.ascii_uppercase)
