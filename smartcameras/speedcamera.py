3bd679c5fd0810b4ed558d59e20546a9e47ad698
Pedro Pinto da Silva (PGR) <ppintodasilva@gmail.com>
2016-12-08 15:48:57 +0000

    Implemented activate and deactivate. Tested successfully with threads.

--- smartcameras/speedcamera.py
+++ smartcameras/speedcamera.py
@@ -10,37 +10,46 @@ class SpeedCamera(object):
         self.id = uuid.uuid4()
         self.street = street
         self.city = city
+        self.isActivated = False
+        self.speedLimit = None
+        self.rate = None
 
-    def relocate(self, street, city):
+    def relocate(self, street, city = None):
         self.street = street
-        selt.city = city
+        if(city is not None):
+            self.city = city
 
     def activate(self, speedLimit, rate):
-        if(self.isActivated is None or self.isActivated is True):
-            raise EnvironmentError('Speed camera is already active. Deactivate first.')
+        if(self.isActivated is True):
+            raise EnvironmentError('Speed camera is already active: deactivate first.')
         self.speedLimit = speedLimit
         self.rate = rate
         self.datetime = datetime.datetime.now()
         self.isActivated = True
-        self.nextVehicle = threading.Event()
-        # Inform Azure
+        # Inform Azure of activated camera
         self.__notifyAzureOfSelf()
-        # Loop
+        # Event representing the passing of the next vehicle
+        self.nextVehicle = threading.Event()
+        # Loop until deactivate is called
+        # (preferably from a separate thread/process!!!!)
         while self.isActivated:
-            nextArrival = self.__genNextArrival(rate)
-            nextVehicle.wait(timeout=nextArrival):
+            nextArrival = self.__genNextArrival()
+            self.nextVehicle.wait(timeout=nextArrival)
             # Vehicle has passed - Create new vehicle
             self.__onObservedVehicle()
         # End of Loop
 
+    #
     def deactivate(self):
         self.isActivated = False
         self.nextVehicle.set()
 
     ## Helping/Private methods
 
-    def __genNextArrival(self, rate):
-        return np.random.poisson(rate, 1)
+    def __genNextArrival(self):
+        if(self.rate is None):
+            raise ValueError("Rate is undefined")        
+        return np.random.exponential(1./self.rate)
 
     def __notifyAzureOfSelf(self):
         print "Notifying Azure of Self"
--- smartcameras/test/test_camera.py
+++ smartcameras/test/test_camera.py
@@ -1,2 +1,32 @@
+import threading
+from smartcameras.speedcamera import SpeedCamera
+import time
+
 def test_constructor():
-    assert True;
+    camera = SpeedCamera("Blandford Square", "Newcastle")
+    assert camera.street == "Blandford Square"
+    assert camera.city == "Newcastle"
+
+def test_uuids():
+    camera1 = SpeedCamera("Blandford Square", "Newcastle")
+    camera2 = SpeedCamera("Blandford Square", "Newcastle")
+    assert camera1.id != camera2.id
+
+def test_relocate():
+    camera = SpeedCamera("Blandford Square", "Newcastle")
+    camera.relocate("St. James Avenue")
+    assert camera.street == "St. James Avenue"
+    camera.relocate("Campo Alegre", "Porto")
+    assert camera.street == "Campo Alegre"
+    assert camera.city == "Porto"
+
+def test_activity_with_threads():
+    print ""
+    camera = SpeedCamera("Blandford Square", "Newcastle")
+    t = threading.Thread(target=camera.activate, args=(50, 5))
+    t.start()
+    time.sleep(5)
+    assert camera.isActivated == True
+    camera.deactivate()
+    t.join()
+    assert camera.isActivated == False
