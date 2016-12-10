e87858536a315e068df42bdbf1390bae7a3dea4d
Pedro Pinto da Silva (PGR) <ppintodasilva@gmail.com>
2016-12-10 14:30:52 +0000

    Added optional name to constructor

--- smartcameras/speedcamera.py
+++ smartcameras/speedcamera.py
@@ -3,16 +3,20 @@ import datetime
 import time
 import numpy as np
 import threading
+import vehicle
 
 class SpeedCamera(object):
 
-    def __init__(self, street, city):
+    def __init__(self, street, city, cloudhook, name = None):
         self.id = uuid.uuid4()
         self.street = street
         self.city = city
         self.isActivated = False
         self.speedLimit = None
         self.rate = None
+        self.cloudhook = cloudhook
+        if name is not None:
+            self.name = name
 
     def relocate(self, street, city = None):
         self.street = street
@@ -27,7 +31,7 @@ class SpeedCamera(object):
         self.datetime = datetime.datetime.now()
         self.isActivated = True
         # Inform Azure of activated camera
-        self.__notifyAzureOfSelf()
+        self.__notifyCloudOfSelf()
         # Event representing the passing of the next vehicle
         self.nextVehicle = threading.Event()
         # Loop until deactivate is called
@@ -48,17 +52,18 @@ class SpeedCamera(object):
 
     def __genNextArrival(self):
         if(self.rate is None):
-            raise ValueError("Rate is undefined")        
+            raise ValueError("Rate is undefined")
         return np.random.exponential(1./self.rate)
 
-    def __notifyAzureOfSelf(self):
+    def __notifyCloudOfSelf(self):
         print "Notifying Azure of Self"
         return False
 
-    def __notifyAzureOfVehicle(self, vehicle):
+    def __notifyCloudOfVehicle(self, vehicle):
         print "Sending vehicle detection to Azure"
         return False
 
     def __onObservedVehicle(self):
-        print "Woooooooooooooo -  A new vehicle just passed by"
-        return False
+        # print "Woooooooooooooo -  A new vehicle just passed by"
+        aVehicle = vehicle.NormalVehicle(self.speedLimit)
+        self.__notifyCloudOfVehicle(aVehicle)
