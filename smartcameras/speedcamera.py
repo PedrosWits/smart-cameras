afdaca8895208ec6a4641a4cf7bb37f1b9ffca57
Pedro Pinto da Silva (PGR) <ppintodasilva@gmail.com>
2016-12-11 18:21:01 +0000

    Integrated with azure hook

--- smartcameras/speedcamera.py
+++ smartcameras/speedcamera.py
@@ -4,17 +4,19 @@ import time
 import numpy as np
 import threading
 import vehicle
+import azurehook
 
 class SpeedCamera(object):
 
-    def __init__(self, street, city, cloudhook, name = None):
+    def __init__(self, street, city, cloudhook = None, name = None):
         self.id = uuid.uuid4()
         self.street = street
         self.city = city
-        self.isActivated = False
+        self.isActive = False
         self.speedLimit = None
         self.rate = None
-        self.cloudhook = cloudhook
+        if cloudhook is None:
+            self.cloudhook = azurehook.AzureHook()
         if name is not None:
             self.name = name
 
@@ -23,39 +25,41 @@ class SpeedCamera(object):
         if(city is not None):
             self.city = city
 
+    # Most commonly executes on its own thread
     def activate(self, speedLimit, rate):
-        if(self.isActivated is True):
+        if(self.isActive is True):
             raise EnvironmentError('Speed camera is already active: deactivate first.')
         self.speedLimit = speedLimit
         self.rate = rate
         self.datetime = datetime.datetime.now()
-        self.isActivated = True
+        self.isActive = True
         # Inform Azure of activated camera
         self.__notifyCloudOfSelf()
         # Event representing the passing of the next vehicle
         self.nextVehicle = threading.Event()
         # Loop until deactivate is called
         # (preferably from a separate thread/process!!!!)
-        while self.isActivated:
+        while self.isActive:
             nextArrival = self.__genNextArrival()
             self.nextVehicle.wait(timeout=nextArrival)
             # Vehicle has passed - Create new vehicle
             self.__onObservedVehicle()
         # End of Loop
 
-    #
+    # Preferably called from a separate thread
     def deactivate(self):
-        self.isActivated = False
+        self.isActive = False
         self.nextVehicle.set()
 
     ## Helping/Private methods
-
+    ################################################
     def __genNextArrival(self):
         if(self.rate is None):
             raise ValueError("Rate is undefined")
         return np.random.exponential(1./self.rate)
 
     def __notifyCloudOfSelf(self):
+
         print "Notifying Azure of Self"
         return False
 
@@ -67,3 +71,15 @@ class SpeedCamera(object):
         # print "Woooooooooooooo -  A new vehicle just passed by"
         aVehicle = vehicle.NormalVehicle(self.speedLimit)
         self.__notifyCloudOfVehicle(aVehicle)
+
+
+# def main():
+#     parser = argparse.ArgumentParser(
+#         description='Launch a speed camera')
+#     parser.add_argument('action',
+#                         metavar="<action>",
+#                         choices=['start', 'shutdown', 'status', 'camera'],
+#                         help='%(choices)s')
+#
+# if __name__ == "__main__":
+#     main()
