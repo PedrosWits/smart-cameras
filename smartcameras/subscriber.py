cd7c5aeec1f5761fe1ebf14340b5df3b698f47f2
Pedro Pinto da Silva (PGR) <ppintodasilva@gmail.com>
2016-12-13 18:50:03 +0000

    Implemented and tested VehicleInspector

--- smartcameras/subscriber.py
+++ smartcameras/subscriber.py
@@ -1,9 +1,22 @@
 from abc import ABCMeta, abstractmethod
+from azure.servicebus import Rule
+from speedcamera import SpeedCamera
 import azurehook
 import threading
 import json
 import math
+import time
+import Queue
+import random
 
+################################################################################
+################################################################################
+#
+#      A Generic Class responsible for handling data received
+#     through an Azure subscription
+#
+################################################################################
+################################################################################
 class AzureSubscriber(object):
     __metaclass__ = ABCMeta
 
@@ -43,6 +56,10 @@ class AzureSubscriber(object):
     def terminate(self):
         self.isActive = False
         self.nextCheck.set()
+        self.onTerminate()
+
+    def onTerminate(self):
+        pass
 
     # Initially Amortized exponential backoff:
     # Given by the formula: timeout(seconds) = (2^ntries / 10)
@@ -57,3 +74,59 @@ class AzureSubscriber(object):
     @abstractmethod
     def onNewMessage(self, dic):
         pass
+
+
+################################################################################
+################################################################################
+#
+#     Class responsible for vehicle checks
+#
+################################################################################
+################################################################################
+
+class VehicleInspector(AzureSubscriber):
+
+    def __init__(self, queueSize = 0):
+        rule = Rule()
+        rule.filter_type = 'SqlFilter'
+        rule.filter_expression = "event = '%s'" % (SpeedCamera.EVENT_VEHICLE)
+        # Call super class constructor
+        AzureSubscriber.__init__(self, SpeedCamera.TOPIC, "VehicleInspector",
+                                 "VehicleInspectorRule", rule)
+        # Thread runs isVehicleStolen which processes items from queue
+        # (blocks until one is available)
+        processingThread = threading.Thread(target=self.vehicleProcessing)
+        # If parent thread exists this one exits too
+        processingThread.daemon = True
+        processingThread.start()
+
+    def onNewMessage(self, dic):
+        plate = dic['vehicle']['plate']
+        self.queue.put(plate, block = True, timeout = 10)
+        # print("Size of queue (after put) =  %d" % self.queue.qsize())
+
+    # This will be killed when parent thread is killed
+    def vehicleProcessing(self):
+        # Instantiate queue
+        self.queue = Queue.Queue()
+        print("")
+        print("Vehicle processing started (queue size =  %d)" % self.queue.qsize())
+        while True:
+            plate = self.queue.get(block = True, timeout = None)
+            isStolen = self.isVehicleStolen(plate)
+            if isStolen:
+                print "Vehicle with plate '%s' is stolen!!!" % plate
+            else:
+                print "Vehicle with plate '%s' is NOT stolen." % plate
+
+    # Should be called from a separate thread
+    def isVehicleStolen(self, plate, sleepFor = 5):
+        print ""
+        print "Processing vehicle with plate: '%s'" % plate
+        time.sleep(sleepFor)
+        return random.random() > 0.95
+
+    def onTerminate(self):
+        print "Killed"
+        print ""
+        print("Size of queue =  %d" % self.queue.qsize())
